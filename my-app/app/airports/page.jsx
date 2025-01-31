9409244a754d9c980ce60bd84a569ce8368ebe97
Kah Yan Tang <s10267338@connect.np.edu.sg>
2025-01-31 19:24:26 +0800

    - added a carousel container such that there is a margin between the carousel and the body.

--- my-app/app/airports/page.jsx
+++ my-app/app/airports/page.jsx
@@ -10,37 +10,35 @@ export default function App() {
     <section>
     <h1 id="header">Best Airports in the World</h1>
     </section>
-
+    
+    <div className="carouselContainer">
     <Carousel fade>
+        <Carousel.Item>
+          <b class="rank">No. 1</b>
+            <img id="hamadAirport" src="https://dohahamadairport.com/sites/default/files/styles/content_image_banner/public/news/HIA%20ORCHARD.JPG?itok=sPpjAswV" alt="Hamad International Airport" width="1400" height="850"/>
+            <Carousel.Caption>
+              <h1 className="airportName">Hamad International Airport</h1>
+              <p className="airportDescription">State-of-the-art facilities, exceptional passenger experience, and seamless connectivity.</p>
+            </Carousel.Caption>
+        </Carousel.Item>
       <Carousel.Item>
-        <div className="carouselImage">
-          <img id="hamadAirport" src="https://dohahamadairport.com/sites/default/files/styles/content_image_banner/public/news/HIA%20ORCHARD.JPG?itok=sPpjAswV" alt="Hamad International Airport" width="1400" height="820"/>
-        </div>
+        <b class="rank">No. 2</b>
+          <img src="/images/changi.jpg" alt="Singapore Changi Airport" width="1400" height="850"/>
           <Carousel.Caption>
-            <h1 className="airportName">Hamad International Airport</h1>
-            <p className="airportDescription">State-of-the-art facilities, exceptional passenger experience, and seamless connectivity.</p>
+            <h1 className="airportName">Singapore Changi Airport</h1>
+            <p className="airportDescription">Innovative amenities, lush gardens, and world-class passenger experience.</p>
           </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <div className="carouselImage">
-          <img src="/images/changi.jpg" alt="Singapore Changi Airport" width="1400" height="820"/>
-        </div>
-        <Carousel.Caption>
-          <h1 className="airportName">Singapore Changi Airport</h1>
-          <p className="airportDescription">Innovative amenities, lush gardens, and world-class passenger experience.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="carouselImage">
-          <img id="incheonAirport"src="/images/incheon.jpg" alt="Seoul Incheon Airport" width="1400" height="820"/>
-        </div>
-        <Carousel.Caption>
-          <h1 className="airportName">Seoul Incheon Airport</h1>
-          <p className="airportDescription">Exceptional service quality, advanced facilities, and seamless passenger experience.</p>
-        </Carousel.Caption>
+        <b class="rank">No. 3</b>
+          <img id="incheonAirport"src="/images/incheon.jpg" alt="Seoul Incheon Airport" width="1400" height="850"/>
+          <Carousel.Caption>
+            <h1 className="airportName">Seoul Incheon Airport</h1>
+            <p className="airportDescription">Exceptional service quality, advanced facilities, and seamless passenger experience.</p>
+          </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-
+    </div>
     </>
   );
 }
