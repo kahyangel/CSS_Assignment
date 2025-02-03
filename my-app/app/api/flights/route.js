0bc9d950b06e2d8b429f62c2aef2b46c3cab716b
Camellia Ong <cam.ongjs@gmail.com>
2025-02-03 23:46:50 +0800

    made api for pricing

---
+++ my-app/app/api/flights/route.js
@@ -0,0 +1,51 @@
+export async function GET(request) {
+    return new Response.json ({
+        "destination": [
+            {
+                "japan": [
+                    {
+                        "airline": "Japan Airlines",
+                        "price": 653
+                        
+                    },
+                    {
+                        "airline": "Singapore Airlines",
+                        "price": 935
+                    },
+                    {
+                        "airline": "Air Japan",
+                        "price": 637
+                    }
+                ],
+                "korea": [
+                    {  
+                        "airline": "Korean Air",
+                        "price": 746
+                    },
+                    {
+                        "airline": "Asiana Airlines",
+                        "price": 503
+                    },
+                    {
+                        "airline": "Singapore Airlines",
+                        "price": 873
+                    }
+                ],
+                "china": [
+                    {
+                        "airline": "China Eastern Airlines",
+                        "price": 836
+                    },
+                    {
+                        "airline": "Air China",
+                        "price": 746
+                    },
+                    {
+                        "airline": "Singapore Airlines",
+                        "price": 928
+                    }
+                ]
+            }
+        ]
+    })
+}
