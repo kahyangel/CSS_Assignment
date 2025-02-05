a432cf6d75b8f76a7bc76d6811580172261e11bc
You Tong Liew <s10268015@connect.np.edu.sg>
2025-02-06 00:57:48 +0800

    created the country timezone finder

--- my-app/app/timezone/page.jsx
+++ my-app/app/timezone/page.jsx
@@ -1,7 +1,11 @@
 export default function App() {
     return (
         <>
-        {/*Add your code here*/}
+        <title>Find the timezone and current time of the country</title>
+        <section>
+            <hi id="header">Find timezone and current time</hi>
+        </section>
+        
         </>
     )
 };
