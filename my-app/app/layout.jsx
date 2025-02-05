a73e52c82dd3be6b9eb30cd8b21c31b6777cdbff
Kah Yan Tang <s10267338@connect.np.edu.sg>
2025-02-06 00:24:26 +0800

    Removed the Nav.Link function from the search and menu icons

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -1,14 +1,32 @@
+/*
+RootLayout Component (Kah Yan)
+-------------------------------------------------------------------------------------------------------------------------------
+Defined the RootLayout component, which includes a responsive navigation bar using Bootstrap's built-in Navbar component,
+which is customised with our navigation links and icons from react-icons library
+-------------------------------------------------------------------------------------------------------------------------------
+*/
+
+// Enables client-side rendering for this component
 "use client";
 
+// Import font from Google
 import { Inter } from 'next/font/google'
+
+// Import global CSS styles
 import './globals.css'
+
+// Import global CSS styles
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+
+// Import icons from react-icons library
 import { FaSearch, FaBars } from "react-icons/fa";
 
+// Configuring the Inter font for consistent typography
 const inter = Inter({ subsets: ['latin'] })
 
+// RootLayout component: Defines the root layout structure of the application
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -24,13 +42,14 @@ export default function RootLayout({ children }) {
             <Nav.Link href="airports">Best Airports</Nav.Link>
             <Nav.Link href="timezone">Time Zones</Nav.Link>
             <Nav.Link href="pricing">Pricing</Nav.Link>
-            <Nav.Link href="search">
-                <FaSearch size={18} />
-              </Nav.Link>
-              <Nav.Link href="#menu">
-                <FaBars size={20} />
-              </Nav.Link>
           </Nav>
+
+          <div style={{ marginLeft: '15px', color: 'white'}}>
+            <FaSearch size={18} />
+          </div>
+          <div style={{ marginLeft: '15px', color: 'white'}}>
+            <FaBars size={20} />
+          </div>
         </Container>
       </Navbar>
     </>
