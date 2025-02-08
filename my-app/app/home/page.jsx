b30d1070c0ace1cb385868e8f52b5d1309195e7c
Si Jia Kwoh <s10267641@connect.np.edu.sg>
2025-02-08 21:50:25 +0800

    editted minor changes page.module.css

--- my-app/app/home/page.jsx
+++ my-app/app/home/page.jsx
@@ -11,38 +11,28 @@ export default function Page() {
         <div className={styles.branding}>
           WORLD 🗺️⁀જ✈︎<br /> 🛫 TRAVELLER
         </div>
-        <h1>Your Perfect Travel Guide!</h1>
+        <div className={styles.tagline}>Your Perfect Travel Guide!</div>
       </div>
 
       {/* Main Content */}
-      <div className={styles.tagline}>Explore the World with Ease</div>
       <div className={styles.topSection}>
         {/* Left Section */}
         <div className={styles.leftSection}>
           <h2 className={styles.header}>World's Best Airports</h2>
-          <p>Discover the world's best airports</p>
-          <a href="/airports" className={styles.link}>
-            Learn More
-          </a>
+          <a href="/airports" className={styles.link}>Discover the world's best airports</a>
         </div>
 
         {/* Right Section */}
         <div className={styles.rightSection}>
           <h2 className={styles.header}>World Clock</h2>
-          <p>Prepare yourself for jetlag</p>
-          <a href="/timezone" className={styles.link}>
-            Learn More
-          </a>
+          <a href="/timezone" className={styles.link}>Prepare yourself for jetlag</a>
         </div>
       </div>
 
       {/* Bottom Section */}
       <div className={styles.bottomSection}>
         <h2 className={styles.header}>Compare Prices</h2>
-        <p>Find yourself the best deals</p>
-        <a href="/pricing" className={styles.link}>
-          Learn More
-        </a>
+        <a href="/pricing" className={styles.link}>Find yourself the best deals</a>
       </div>
     </div>
   );
