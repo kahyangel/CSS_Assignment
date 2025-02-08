483abb3392de13e4ae7205f428b61899471b36de
Si Jia Kwoh <s10267641@connect.np.edu.sg>
2025-02-08 22:37:01 +0800

    added wonderfall page

---
+++ my-app/app/wonderfall/page.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './page.module.css';
+
+export default function WonderfallPage() {
+  return (
+    <div className={styles.container}>
+      {/* Left Section: Image */}
+      <div className={styles.leftSection}>
+        <img
+          src="/images/wonderfall2.jpg" 
+          alt="Wonderfall at Changi"
+          className={styles.image}
+        />
+      </div>
+
+      {/* Right Section: Factual Statements */}
+      <div className={styles.rightSection}>
+        <h1 className={styles.header}>Wonderfall at Changi</h1>
+        {/* <p className={styles.text}>The Wonderfall at Changi Airport is a breathtaking indoor waterfall that is the tallest of its kind in the world.</p>
+        <p className={styles.text}>Located in Jewel Changi, it stands at an impressive height of 40 meters, cascading from the center of a stunning glass dome.</p>
+        <p className={styles.text}>Surrounded by lush greenery, the Wonderfall creates a mesmerizing experience for travelers and visitors alike.</p>
+        <p className={styles.text}>It is a prime example of Singapore’s innovation and dedication to creating world-class attractions.</p> */}
+        <p className={styles.text}>Behold an iconic landmark built with nature-inspired design and digital wizardry in the departure hall of the new Terminal 2. This digital waterfall cascades among lush green walls to create a mesmerizing, calming spectacle.</p>
+        <p></p>
+        <p className={styles.text}>Within The Wonderfall lies a melodic surprise – Rhythms of Nature, an abstract ballet of human design and Mother Nature's harmonious dance. Guided by an original composition from neo-classical pianist-composer, Jean-Michel Blais, experience a symphonic masterpiece where design and nature awaits.</p>
+      </div>
+    </div>
+  );
+}
